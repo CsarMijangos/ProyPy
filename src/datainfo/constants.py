b3b6ce3adf68a12bd6cc9e46e84591a6be0199c3
CsarMijangos <csarmijangos496@gmail.com>
2022-12-05 18:10:33 -0600

    Update

--- src/datainfo/constants.py
+++ src/datainfo/constants.py
@@ -3,49 +3,52 @@
 
 # The precharged example:
 
-CENTRAL_DB = "data/data_bases/example/central.fasta"    #Enzime family DB
-GENOMES_DB = "data/data_bases/example/17genomes.fasta"        #Genome DB
-NAT_PRODS_DB = "data/data_bases/example/nat_prods.fasta"         #Natural products DB (default =MIBiG)
+EXMPL_CENTRAL_DB = "data/data_bases/example/central/central.fasta"    #Enzime family DB
+EXMPL_GENOMES_DB = "data/data_bases/example/genomes/17genomes.fasta"  #Genome DB
+MIBiG_NAT_PRODS_DB = "data/data_bases/example/mibig/nat_prods.fasta"  #Natural products DB(default =MIBiG)
 
 
-# The user databases
+## The user databases
 
-USER_CENTRAL_DB = "data/data_bases/user_data_bases/"
-USER_GENOMES_DB = "data/data_bases/user_data_bases/"
-USER_NAT_PRODS_DB = "data/data_bases/user_data_bases/"
+USER_CENTRAL_DB = "data/data_bases/user_data_bases/central/"
+USER_GENOMES_DB = "data/data_bases/user_data_bases/genomes/"
+USER_NAT_PRODS_DB = "data/data_bases/user_data_bases/nat_prods/"
 
 
 ## The following are the paths to the blast databases files:
 
-##The precharged blast databases example:
-
-CENTRAL_BLAST_DB = "data/blast_dbs/example/central_blastdb"
-GENOMES_BLAST_DB = "data/blast_dbs/example/17genomes_blastdb"
-NAT_PRODS_BLAST_DB = "data/blast_dbs/example/at_prods_blastdb"
+CENTRAL_BLAST_DB = "data/blast_dbs/central/central_blastdb"
+GENOMES_BLAST_DB = "data/blast_dbs/genomes/17genomes_blastdb"
+NAT_PRODS_BLAST_DB = "data/blast_dbs/nat_prods/nat_prods_blastdb"
 
 
 #The user blast databases:
 
-USER_CENTRAL_BLAST_DB = "data/blast_dbs/user_blastdbs/"
-USER_GENOMES_BLAST_DB = "data/blast_dbs/user_blastdbs/"
-USER_NAT_PRODS_BLAST_DB = "data/blast_dbs/user_blastdbs/"
+#USER_CENTRAL_BLAST_DB = "data/blast_dbs/user_blastdbs/"
+#USER_GENOMES_BLAST_DB = "data/blast_dbs/user_blastdbs/"
+#USER_NAT_PRODS_BLAST_DB = "data/blast_dbs/user_blastdbs/"
 
 ## The following are the paths to the output files of blastp:
 
-## The precharged example:
+CENTRALvsGNMs_blastp = "data/blastp_files/central_to_genomes/blast_1.blast" # blastp of Enzime family DB to Genomes DB 
+GNMsvsCENTRAL_blastp = "data/blastp_files/genomes_to_central/blast_2.blast" #blastp of GNM vs EF 
+EXP_FAMSvsNAT_PRODS_blastp = "data/blastp_files/expFam_to_natProds/blast_3.blast"   # blastp of expanded families vs
+                                                                        # natural products 
 
-CENTRALvsGNMs_blastp = "data/blastp_files/example/blast_1.blast" # blastp of Enzime family DB to Genomes DB 
-GNMsvsCENTRAL_blastp = "data/blastp_files/example/blast_2.blast" #blastp of GNM vs EF 
-EXP_FAMSvsNAT_PRODS_blastp = "data/blastp_files/example/blast_3.blast"   # blastp of expanded families vs
-                                   # natural products 
-#The user case:
 
-USER_CENTRALvsGNMs_blastp = "data/blastp_files/user_blastp_files/"
-USER_GNMsvsCENTRAL_blastp = "data/blastp_files/user_blastp_files/"
-USER_EXP_FAMSvsNAT_PRODS_blastp = "data/blastp_files/user_blastp_files/"
 
 ## The following are dictionaries with the ids of the
 ## enzime families, genomes and natural products:
+
+
+
+
+
+#################### cmd's for using blast tools with biopython ########
+CMD_blastp = "/home/csar/ncbi-blast-2.13.0+/bin/blastp"
+CMD_makeblastdb = "/home/csar/ncbi-blast-2.13.0+/bin/makeblastdb"
+
+
 
 GNMs_ids = {}
 CENTRAL_ids = {}
--- src/datainfo/helpers.py
+++ src/datainfo/helpers.py
@@ -1,21 +1,38 @@
-def bitscore_filter(data, threshold):
-    '''This function filters those genomes in data (data = EFvsGNMs_blastp)
-    such that their bitscore is greater or equal to threshold.
-    '''
-    data.query("bitscore >= @threshold", inplace = True)
-    data.reset_index(drop=True, inplace=True)
-    return data
+import constants 
+from Bio.Blast.Applications import NcbiblastpCommandline
+from Bio.Blast.Applications import NcbimakeblastdbCommandline 
 
-def blastpOf(query_path,db_path):
-    '''
-    This function is responsible of all related to the 
-    correct applying of blastp.
-    '''
+
+class EvoMining:
+    def __init__(self, central_path = constants.CENTRAL_DB,
+                genomes_path = constants.GENOMES_DB,
+                nat_prods_path = constants.NAT_PRODS_DB):
+        self.central_path = central_path
+        self.genomes_path = genomes_path
+        self.nat_prods_path = nat_prods_path
     
+    def setup_dbs(self, new_central_path,
+                 new_genomes_path,
+                 new_nat_prods_path):
+        self.central_path = new_central_path
+        self.genomes_path = new_genomes_path
+        self.nat_prods_path = new_nat_prods_path
+    
+    def _makeblast_DBs(self):
+        list_dbs = [self.central_path, self.genomes_path, self.nat_prods_path]
+        lista_names = ["central", "genomes", "nat_prods"]
+        if list_dbs == [constants.CENTRAL_DB, constants.GENOMES_DB, constants.NAT_PRODS_DB]:
+            for db in list_dbs:
+                cline_mkdb = NcbimakeblastdbCommandline(cmd = constants.CMD_makeblastdb,
+                                                dbtype="prot",
+                                                input_file= db,
+                                                out = "data/blast_dbs/example/"+lista_names)
 
-def makeblast_db(db_path):
-    '''
-     
-    '''
+
+
+
+
+
+    
     
     
